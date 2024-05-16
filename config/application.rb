8e5693a20fe87818d1e4ffbc10578ea4339f6c49
Sushil Subedi <sushilsubedi151@gmail.com>
2024-05-16 14:08:59 +0545

    updated application to verify token

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,8 @@ module SpreeStarter
 
     config.time_zone = 'Central Time (US & Canada)'
 
+    puts "token======> #{ENV["SECRET_KEY_BASE"]}"
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
