11a01ccfcd2fed119b0f24fdc7f373dbef40bafd
Sushil Subedi <sushilsubedi151@gmail.com>
2024-05-27 09:01:58 +0545

    updated configuration for checking if user exist or not

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 set -e
 bundle exec rails db:migrate
-printf 'spree@example.com\nspree123\n' | RAILS_ENV=production bundle exec rake db:seed
+# Check if an admin user already exists
+admin_exists=$(bundle exec rails runner "puts Spree::User.exists?(email: 'spree@example.com')")
+
+# Seed the database if no admin user exists
+if [ "$admin_exists" = "false" ]; then
+  printf 'spree@example.com\nspree123\n' | RAILS_ENV=production bundle exec rake db:seed
+else
+  echo "Admin user already exists, skipping seeding."
+fi
 exec "$@"
